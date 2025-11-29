0551f860ca6dea6dfd1b6ee348801f17e8eeef88
Minnie Thuo <minniethuo@protonmail.com>
2025-11-29 23:25:10 +0300

    add calculator

---
+++ PLP-assignments/calculator.py
@@ -0,0 +1,19 @@
+first_number = float(input("Enter a number"))
+second_number = float(input("Enter another number"))
+operator = input("Enter a mathematical operator")
+
+if operator == "+":
+    result = first_number + second_number
+    print(result) 
+elif operator == "-":
+    result = first_number + second_number
+    print(result) 
+elif operator == "*":
+    result = first_number + second_number
+    print(result) 
+elif operator == "/":
+    result = first_number + second_number
+    print(result) 
+else:
+    print("ERROR!")
+
